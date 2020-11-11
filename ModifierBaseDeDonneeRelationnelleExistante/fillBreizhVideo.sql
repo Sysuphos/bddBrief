0c322688227b649ff14ed2d42f3b1ef69fd2e4fc
Sysuphos <yannick.lajdecki@gmail.com>
2020-11-11 23:15:33 +0100

    Brief BDD 1 et 2

---
+++ ModifierBaseDeDonneeRelationnelleExistante/fillBreizhVideo.sql
@@ -0,0 +1,15 @@
+INSERT INTO breizhvideo.actor
+SELECT * FROM sakila.actor;
+
+INSERT INTO breizhvideo.category
+SELECT * FROM sakila.category;
+
+INSERT INTO breizhvideo.film (title, description, release_year,length,special_features,last_update)
+SELECT title, description, release_year,length,special_features,last_update
+FROM sakila.film;
+
+INSERT INTO breizhvideo.film_actor
+SELECT * FROM sakila.film_actor;
+
+INSERT INTO breizhvideo.film_category
+SELECT * FROM sakila.film_category;
